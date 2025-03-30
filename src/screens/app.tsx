00a0ca1b62aa35a5ae386bef84628a504767fc35
ko-shiro-hap <koushirou.iino@icloud.com>
2025-03-30 15:47:07 +0900

    add: アルバムから画像選択機能を追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,6 +1,7 @@
 import { type FC } from "react";
 import { StyleSheet, View } from "react-native";
 import { registerRootComponent } from "expo";
+import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
 
 import PlaceholderImage from "@/assets/images/background-image.png";
@@ -8,13 +9,26 @@ import { Button } from "@/components/button";
 import { ImageViewer } from "@/components/image-viewer";
 
 const App: FC = () => {
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("画像が選択されていません");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="写真を選択" />
+        <Button theme="primary" label="写真を選択" onPress={pickImageAsync} />
         <Button label="この写真を使用" />
       </View>
       <StatusBar style="auto" />
